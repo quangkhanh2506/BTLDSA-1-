19ac5b12c2d78982703ac5dd56b2e9a92f223678
quangkhanh2506 <80532113+quangkhanh2506@users.noreply.github.com>
2022-10-07 22:14:11 +0700

    gần xong

--- ConcatStringList.cpp
+++ ConcatStringList.cpp
@@ -3,3 +3,95 @@
 ConcatStringList::ReferencesList ConcatStringList::refList = ConcatStringList::ReferencesList();
 ConcatStringList::DeleteStringList ConcatStringList::delStrList = ConcatStringList::DeleteStringList();
 
+ConcatStringList::ConcatStringList(const char* s){
+    this->Data = new CharALNode(s);
+    refList.insertCharAlNode(this->Data);
+}
+int ConcatStringList::length() const{
+    int length=0;
+    if(this->Data->head!=nullptr){
+        CharALNode* cur=this->Data->head;
+        while (cur!=nullptr)
+        {
+            length+=(int)strlen(cur->CharArrayList);
+            cur=cur->next;
+        }
+    }
+    return length;
+}
+char ConcatStringList::get(int index) const{
+    if(index<0 || index >= (this->length() - 1) ) throw out_of_range("Index of string is invalid!");
+    CharALNode* cur=this->Data->head;
+    while (cur!=nullptr)
+    {
+        if(index>((int)strlen(cur->CharArrayList)-1)){
+            index-= (int)strlen(cur->CharArrayList);
+            cur=cur->next;
+            continue;
+        }
+        return cur->CharArrayList[index];
+    }
+}
+int ConcatStringList::indexOf(char c) const{
+    if(this->Data->head!=nullptr){
+        CharALNode* cur=this->Data->head;
+        while (cur!=nullptr)
+        {
+            for(int i=0;i<(int)strlen(this->Data->CharArrayList);i++){
+                if(this->Data->CharArrayList[i]==c) return i;
+            }
+            cur=cur->next;
+        }
+    }
+    return -1;
+}
+std::string ConcatStringList::toString() const{
+    std::string totalStr="ConcatStringList[";
+    if(this->Data->head!=nullptr){
+        CharALNode* cur=this->Data->head;
+        while (cur!=nullptr)
+        {
+            totalStr+=cur->CharArrayList;
+            cur=cur->next;
+        }
+    }
+    totalStr+="]";
+    return totalStr;
+}
+ConcatStringList ConcatStringList::concat(const ConcatStringList &otherS) const{
+    ConcatStringList concatstr (this->Data->head->CharArrayList);
+    concatstr.Data->insert(otherS.Data->head->CharArrayList);
+    ReferencesList* cur=refList;
+    while (cur!=nullptr)
+    {
+        if(cur->Refdata->CharArrayList==this->Data->head->CharArrayList){
+            cur->Count_address_of=3;
+        }
+        if(cur->Refdata->CharArrayList==otherS->Data->head->CharArrayList){
+            cur->Count_address_of=3;
+        }
+        cur=cur->next;
+    }
+    return concatstr;
+}
+ConcatStringList ConcatStringList::subString(int from, int to) const{
+    if(from>=to) throw logic_error("Invalid range");
+    if(from<0||to>=this->length()) throw out_of_range("Index of string is invalid");
+    ConcatStringList rtn;
+    
+}
+ConcatStringList ConcatStringList::reverse() const{
+
+}
+ConcatStringList::~ConcatStringList(){
+    if(this->Data->head->next==nullptr){
+        CharALNode* cur=this->Data;
+        while (cur!=nullptr)
+        {
+            if(cur->head->CharArrayList==refList.Refdata->CharArrayList)
+        }
+        
+        delete this->Data->tail;
+        delete this->Data->head;
+    }
+}
--- ConcatStringList.h
+++ ConcatStringList.h
@@ -16,7 +16,7 @@ public:
     class CharALNode
     {
     public:
-        std::string CharArrayList;
+        const char*  CharArrayList;
         CharALNode* next;
         CharALNode* head;
         CharALNode* tail;
@@ -25,13 +25,19 @@ public:
             this->CharArrayList=nullptr;
             this->head=nullptr;
             this->tail=nullptr;
-            this->next=nullptr;
+            this->head->next=nullptr;
         }
-        CharALNode(std::string inp){
+        CharALNode(const char* inp){
+                this->head->CharArrayList=inp;
+                this->head->next=nullptr;
+                this->tail=this->head;
+        }
+        void insert(const char* inp){
             if(this->head==nullptr){
                 this->head->CharArrayList=inp;
+                this->head->next=nullptr;
                 this->tail=this->head;
-                this->next=nullptr;
+                
             }
             else{
                 while (this->tail!=nullptr)
@@ -39,14 +45,14 @@ public:
                     this->tail=this->tail->next;
                 }
                 this->tail->CharArrayList=inp;
-                 
+                this->tail->next=nullptr;
             }
         }
-        CharALNode(std::string inp, CharALNode* NEXT){
+        CharALNode ReCharALNode(char* inp, CharALNode* NEXT){
             if(this->head==nullptr){
-                this->head->CharArrayList=inp;
-                this->next=NEXT;
-                CharALNode* cur=this->next;
+                this->head->CharArrayList = inp;
+                this->head->next=NEXT;
+                CharALNode* cur=this->head->next;
                 while (cur->next!=nullptr)
                 {
                     cur=cur->next;
@@ -54,7 +60,7 @@ public:
                 this->tail=cur;
             }
             else{
-                this->tail->next->CharArrayList=inp;
+                this->tail->next = CharALNode(inp);
                 CharALNode* cur=this->tail->next;
                 cur->next=NEXT;
                 while (cur->next!=nullptr)
@@ -63,20 +69,21 @@ public:
                 }
                 this->tail=cur;
             }
+            return this->head;
         }
 
         ~CharALNode();
         
     };
-    
+    CharALNode* Data;
     
 public:
     ConcatStringList(const char * s);
     int length() const;
-    int get(int index) const;
+    char get(int index) const;
     int indexOf(char c) const;
     std::string toString() const;
-    ConcatStringList concat(const ConcatStringList & otherS) const;
+    ConcatStringList concat(const ConcatStringList &otherS) const;
     ConcatStringList subString(int from, int to) const;
     ConcatStringList reverse() const;
     ~ConcatStringList();
@@ -84,16 +91,59 @@ public:
 public:
     class ReferencesList {
         // TODO: may provide some attributes
-
         public:
-            int size() const;
-            int refCountAt(int index) const;
-            std::string refCountsString() const;
+        CharALNode* Refdata;
+        int Count_address_of;
+        CharALNode* next;
+        int size;
+        public:
+            void insertCharAlNode(CharALNode* inp){
+                if(this->Refdata->head->CharArrayList==nullptr){
+                    this->Refdata->insert(inp->head->CharArrayList);
+                    this->next=nullptr;
+                    this->Count_address_of=2;
+                    size++;
+                }
+                else{
+                    ReferencesList* cur = this;
+                    while (cur->next!=nullptr)
+                    {
+                        cur=cur->next
+                    }
+                    cur=cur->next;
+                    cur->Refdata->insert(inp->head->CharArrayList);
+                    cur->next=nullptr;
+                    cur->Count_address_of=2;
+                    size++;
+                }
+            }
+            int size() const{
+                return this->size;
+            }
+            int refCountAt(int index) const{
+                if(index<0||index>=this->size) throw out_of_range("Index of references list is invalid!");
+                ReferencesList* cur=this;
+                while(index--) cur=cur->next;
+                return cur->Count_address_of;
+            }
+            std::string refCountsString() const{
+                ReferencesList* cur = this;
+                std::string rtn= "RefCounts[";
+                while (cur->next!=nullptr)
+                {
+                    rtn+= (48 + cur->Count_address_of);
+                    rtn+=",";
+                    cur = cur->next;
+                }
+                rtn+= (48 + cur->Count_address_of);
+                rtn+="]";
+                return rtn;
+            }
     };
 
     class DeleteStringList {
         // TODO: may provide some attributes
-
+        
         public:
             int size() const;
             std::string totalRefCountsString() const;
